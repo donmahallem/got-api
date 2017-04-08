5698303401a1d00005c26831d4d07b7b6178aa62
donmahallem <m@m.m0k.eu>
2017-04-08 04:08:10 +0200

    c

--- src/router/v1/auth/auth.endpoints.ts
+++ src/router/v1/auth/auth.endpoints.ts
@@ -11,12 +11,12 @@ import {
 export class AuthEndpoints {
 
     static readonly token: express.RequestHandler = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-        if (req.query.hasOwnProperty("type")) {
-            if (req.query.action === "refresh_token" && req.query.hasOwnProperty("refresh_token")) {
+        if (req.body.hasOwnProperty("type")) {
+            if (req.body.action === "refresh_token" && req.body.hasOwnProperty("refresh_token")) {
                 res.send("refresh");
                 res.end();
-            } else if (req.query.action === "code" && req.query.hasOwnProperty("code")) {
-                res.send(req.query);
+            } else if (req.body.action === "code" && req.body.hasOwnProperty("code")) {
+                res.send(req.body.code);
                 res.end();
             } else {
                 next(new Error("no stuff provided"));
