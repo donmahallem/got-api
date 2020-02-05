eb55810ba0fd2c3a3f93892675a0ee27db124f2a
donmahallem <donmahallem@users.noreply.github.com>
2020-02-05 21:04:21 +0100

    fix lint

--- src/router/v1/auth/auth.endpoints.ts
+++ src/router/v1/auth/auth.endpoints.ts
@@ -1,8 +1,12 @@
+/*!
+ * Source https://github.com/donmahallem/got-api
+ */
+
 import * as express from "express";
 import {
-    RedditHelper,
     Auth,
-    RedisApi
+    RedditHelper,
+    RedisApi,
 } from "./../../../util/";
 export class AuthEndpoints {
 
@@ -10,15 +14,14 @@ export class AuthEndpoints {
         if (req.body.hasOwnProperty("type")) {
             if (req.body.type === "refresh_token" && req.body.hasOwnProperty("refresh_token")) {
                 Auth.verify(req.body.refresh_token)
-                    .then(decoded => {
-                        return Promise.all([Auth.createAccessToken(decoded.user.id), Auth.createRefreshToken(decoded.user.id)])
-                    })
+                    .then(decoded =>
+                        Promise.all([Auth.createAccessToken(decoded.user.id), Auth.createRefreshToken(decoded.user.id)]))
                     .then(tokens => {
                         res.json({
                             data: {
                                 access_token: tokens[0],
-                                refresh_token: tokens[1]
-                            }
+                                refresh_token: tokens[1],
+                            },
                         });
                     })
                     .catch(err => {
@@ -26,42 +29,35 @@ export class AuthEndpoints {
                     });
             } else if (req.body.type === "code" && req.body.hasOwnProperty("code")) {
                 RedditHelper.exchangeCode(req.body.code)
-                    .then((resp) => {
-                        return RedditHelper.getMe(resp.access_token)
-                            .then(me => {
-                                return {
-                                    name: me.name,
-                                    id: me.id,
+                    .then((resp) =>
+                        RedditHelper.getMe(resp.access_token)
+                            .then(me =>
+                                ({
                                     access_token: resp.access_token,
-                                    refresh_token: resp.refresh_token
-                                }
-                            });
-                    })
-                    .then(data => {
-                        return RedisApi.storeRedditToken(data.id, data.access_token, data.refresh_token)
-                            .then(success => {
-                                console.log(success);
-                                return data;
-                            });
-                    })
+                                    id: me.id,
+                                    name: me.name,
+                                    refresh_token: resp.refresh_token,
+                                })))
+                    .then(data =>
+                        RedisApi.storeRedditToken(data.id, data.access_token, data.refresh_token)
+                            .then(success => data))
                     .then(data => {
                         const user = {
                             id: data.id,
-                            name: data.name
-                        }
+                            name: data.name,
+                        };
                         return Promise.all([Auth.createAccessToken(user), Auth.createRefreshToken(user)])
-                            .then(results => {
-                                return {
+                            .then(results =>
+                                ({
+                                    access_token: results[0],
                                     id: data.id,
                                     name: data.name,
-                                    access_token: results[0],
-                                    refresh_token: results[1]
-                                }
-                            });
+                                    refresh_token: results[1],
+                                }));
                     })
                     .then(data => {
                         res.json({
-                            "data": data
+                            "data": data,
                         });
                     }).catch(err => {
                         next(err);
